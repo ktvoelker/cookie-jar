2509583f9cc1a0bc1eb672e61e1cac5bd59990a5
Karl Voelker <ktvoelker@gmail.com>
2012-08-24 23:00:02 -0700

    Some date-parsing logic

--- src/Web/CookieJar.hs
+++ src/Web/CookieJar.hs
@@ -6,8 +6,10 @@ module Web.CookieJar
 
 import qualified Data.ByteString as BS
 
+import Control.Monad
 import Data.Maybe
 import Data.Time
+import Data.Word (Word8)
 import Network.HTTP.Types
 
 type Time = UTCTime
@@ -39,6 +41,83 @@ data SetCookie =
 
 parseSetCookie :: Bytes -> Maybe SetCookie
 parseSetCookie bs = undefined
+
+isDateDelim :: Word8 -> Bool
+isDateDelim w =
+     w == 0x09
+  || w >= 0x20 && w <= 0x2F
+  || w >= 0x3B && w <= 0x40
+  || w >= 0x5B && w <= 0x60
+  || w >= 0x7B && w <= 0x7E
+
+colon :: Word8
+colon = 0x3A
+
+-- TODO could this be defined as not . isDateDelim?
+isDateNonDelim :: Word8 -> Bool
+isDateNonDelim w =
+     w >= 0x00 && w <= 0x08
+  || w >= 0x0A && w <= 0x1F
+  || w >= 0x30 && w <= 0x39 -- DIGIT
+  || w == colon
+  || w >= 0x41 && w <= 0x5A -- uppercase ALPHA
+  || w >= 0x61 && w <= 0x7A -- lowercase ALPHA
+  || w >= 0x7F && w <= 0xFF
+
+-- TODO swap the details into isDigit?
+isNonDigit :: Word8 -> Bool
+isNonDigit w =
+     w >= 0x00 && w <= 0x2F
+  || w >= 0x3A && w <= 0xFF
+
+isDigit :: Word8 -> Bool
+isDigit = not . isNonDigit
+
+tokenizeDate :: Bytes -> [Bytes]
+tokenizeDate (BS.dropWhile isDateDelim -> bs)
+  | bs == BS.empty
+    = []
+  | otherwise
+    = let (tok, bs') = BS.span isDateNonDelim bs in tok : tokenizeDate bs'
+
+data DateFields =
+  DateFields
+  { dfTime       :: Maybe (Int, Int, Int)
+  , dfDayOfMonth :: Maybe Int
+  , dfMonth      :: Maybe Int
+  , dfYear       :: Maybe Int
+  } deriving (Show)
+
+digitValue :: Word8 -> Maybe Int
+digitValue w
+  | w >= 0x30 && w <= 0x39
+    = Just $ fromIntegral $ w - 0x30
+  | otherwise
+    = Nothing
+
+digitsValue :: [Word8] -> Maybe Int
+digitsValue = f . reverse . map digitValue
+  where
+    f (Nothing : _) = Nothing
+    f (Just d : ds) = fmap ((+ d) . (* 10)) $ f ds
+
+parseTimeToken :: Bytes -> Maybe (Int, Int, Int)
+parseTimeToken bs = do
+  let [hour, min, secPlus] = map BS.unpack $ take 3 $ BS.split colon bs
+  let sec = takeWhile isDigit secPlus
+  let fs = [hour, min, sec]
+  guard $ all ((>= 1) . length) fs
+  guard $ all ((<= 2) . length) fs
+  let [Just iHour, Just iMin, Just iSec] = map digitsValue fs
+  return (iHour, iMin, iSec)
+
+parseDateToken :: Bytes -> DateFields -> DateFields
+parseDateToken bs df = undefined
+
+parseDate :: Bytes -> DateFields
+parseDate =
+  foldr parseDateToken (DateFields Nothing Nothing Nothing Nothing)
+  . tokenizeDate
 
 receive :: Time -> Endpoint -> SetCookie -> Jar -> Jar
 receive = undefined
